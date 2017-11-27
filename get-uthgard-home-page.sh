a43fca77503fd807703da19a983314915a6372b7
michael campbell <michael.campbell@gmail.com>
2017-11-27 15:26:53 -0500

    removed -t for crontab use

--- get-uthgard-home-page.sh
+++ get-uthgard-home-page.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+exec 1>$HOME/logs/get.1.log
+exec 2>$HOME/logs/get.2.log
+
+
 trap "echo Ending at `date --iso-8601=seconds` >> $HOME/logs/get-uthgard-home-page.sh.log" EXIT
 
 
@@ -12,31 +16,30 @@ mkdir -p $HOME/uthgard/population
 LOG=$HOME/logs/get-uthgard-home-page.sh.log
 OUTPUT=$HOME/uthgard/population/$(/bin/date +%Y-%m-%d)-pj.log
 
-echo Starting at $(date --iso-8601=seconds) >> $LOG
+echo Starting at $(/bin/date --iso-8601=seconds) >> $LOG
 
 ########################################
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   HERE="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
+  SOURCE="$(/bin/readlink "$SOURCE")"
   [[ $SOURCE != /* ]] && SOURCE="$HERE/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 HERE="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-THISBIN="$(basename $0)"
+THISBIN="$(/usr/bin/basename $0)"
 
 cd $HERE
 
 ########################################
-PJ="docker run -it --rm mcampbell/uthgard-pjs"
 
 # [2017-01-20T10:24:49-0500] <@[Uth]Infobot>  � Server Status � Online with 2753 clients and 4 queued clients  �
 #   md = line.match(/.(\d\d\d\d)-(\d\d)-(\d\d).(\d\d):(\d\d).*Server Status . Online with (\d+) clients and (\d+) queued clients/)
-NOW=$(date --iso-8601=seconds)
+NOW=$(/bin/date --iso-8601=seconds)
 
-PLAYERS=$($PJ | \
-            grep Players: | \
-            perl -ne 'print "$1\n" if m|(\d+)</td>|')
+PLAYERS=$(/usr/bin/docker run -i --rm mcampbell/uthgard-pjs | \
+            /bin/grep Players: | \
+            /usr/bin/perl -ne 'print "$1\n" if m|(\d+)</td>|')
 
 echo Got $PLAYERS players >> $LOG
 
